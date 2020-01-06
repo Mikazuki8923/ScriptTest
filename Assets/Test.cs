cebefc610d8ca8ccd4c081b48ae008d52c24188a
mikazuki8923 <mikazuki8923@gmail.com>
2020-01-06 23:11:49 +0900

    ScriptTestプロジェクト　再提出
    基準未達の箇所を修正しました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,68 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
-public class boss {
-    private int mp = 53;　
-
-    //魔法攻撃用の関数
+  
+public class Boss {
+    private int mp = 53;
+    
+    //Magic関数
     public void Magic(int a) {
-        
-        for (int non = this.mp - a; non > -a; non -= a) {
-
-            if (non >= 0) {
-                Debug.Log("魔法攻撃をした。残りMPは" + non + "。");
-            } else {
-                Debug.Log("MPが足りないため魔法が使えない。");
-            }
+        this.mp -= a - (a - 5);
+        if (this.mp > 0) {
+            Debug.Log("魔法攻撃をした。残りmpは" + this.mp + "。");
+        } else {
+            Debug.Log("MPが足りないため魔法が使えない。");
         }
-
-
     }
 }
+
 
 
 public class Test : MonoBehaviour
 {
 
     // Use this for initialization
-    
-    
+    void Start() {
+      
+        Boss lastboss = new Boss();
+        for (int a = 6; a < 17; a++) {
+            lastboss.Magic(a);
+        }
+        
 
-    void Start()  {
         int[] array = new int[5];
 
         array[0] = 50;
-        array[1] = 30;
-        array[2] = 80;
-        array[3] = 60;
-        array[4] = 120;
+        array[1] = 80;
+        array[2] = 100;
+        array[3] = 20;
+        array[4] = 30;
 
-        //順番に表示
-        for(int i = 0; i < array.Length; i++) {  
-            Debug.Log(array [i]);　　　　
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
         }
-      
-        //逆順に表示
-        for(int h = array.Length - 1; h >= 0; h--) {
+        for (int h = array.Length - 1; h >= 0; h--)
+        {
             Debug.Log(array[h]);
         }
-
-
-        boss lastboss = new boss();
-        lastboss.Magic(5);
-       
-        
-
-
 
     }
 
     // Update is called once per frame
     void Update()
     {
+      
 
     }
 }
-
